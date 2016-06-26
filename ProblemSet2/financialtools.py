2c0b82e8697d529757dd9e74c54e1565204f0318
mingangchen <mgchen78@gmail.com>
2016-06-26 20:23:13 +0800

    Update financialtools.py

--- ProblemSet2/financialtools.py
+++ ProblemSet2/financialtools.py
@@ -1,3 +1,12 @@
+'''
+PROBLEM 1: PAYING THE MINIMUM
+Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the credit card company each month.
+'''
+
+#balance - the outstanding balance on the credit card
+#annualInterestRate - annual interest rate as a decimal
+#monthlyPaymentRate - minimum monthly payment rate as a decimal
+
 def payMinimum(balance,annualInterestRate,monthlyPaymentRate):
     month = 0
     totalPaid = 0
@@ -11,3 +20,27 @@ def payMinimum(balance,annualInterestRate,monthlyPaymentRate):
        totalPaid += minPayment
     print 'Total paid:', round(totalPaid,2)
     print 'Remaining balance:', round(balance,2)
+
+
+'''
+PROBLEM 2: PAYING DEBT OFF IN A YEAR
+Now write a program that calculates the minimum fixed monthly payment needed in order pay off a credit card balance within 12 months.
+By a fixed monthly payment, we mean a single number which does not change each month, but instead is a constant amount that will be paid each month.
+'''
+'''
+    balance - the outstanding balance on the credit card
+    annualInterestRate - annual interest rate as a decimal
+'''
+
+def payDebtOff(balance,annualInterestRate):
+    fixed = 10
+    while True:
+        newbalance = balance
+        for month in range(1,13):
+            newbalance -= fixed
+            newbalance += (annualInterestRate/12.0)*newbalance
+        if newbalance > 0:
+            fixed += 10
+        else:
+            break
+    return fixed
